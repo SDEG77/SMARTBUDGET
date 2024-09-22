8ee7c5a14a9186e1005a16db436d858719cc4073
SDEG77 <hahahayeahmannn@gmail.com>
2024-09-22 14:01:48 +0800

    added tables for tracking & its model

---
+++ database/migrations/2024_09_22_054735_create_tracking_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tracking', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(USer::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+            $table->string('mode')->default('ingoing');
+            $table->string('category')->default('unset');
+            $table->string('description')->default('unset');
+            $table->integer('amount')->default(0);
+            $table->date('date')->default('2004/07/07');         
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tracking');
+    }
+};
